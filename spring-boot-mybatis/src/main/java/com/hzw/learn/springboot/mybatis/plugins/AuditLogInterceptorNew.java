3ff7505de4e516dc786da6db7b89495e4c1ef4b4
侯正伟 <houzw@allinfinance.com>
2018-08-14 19:11:14 +0800

    mybatis拦截器

--- spring-boot-mybatis/src/main/java/com/hzw/learn/springboot/mybatis/plugins/AuditLogInterceptorNew.java
+++ spring-boot-mybatis/src/main/java/com/hzw/learn/springboot/mybatis/plugins/AuditLogInterceptorNew.java
@@ -1,5 +1,9 @@
 package com.hzw.learn.springboot.mybatis.plugins;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.ibatis.executor.Executor;
@@ -14,16 +18,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.stereotype.Component;
 @Component
-@ConditionalOnBean(AuditEntitys.class)
+@ConditionalOnBean(name="auditEntitys")
 @Intercepts(@Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class}))
 public class AuditLogInterceptorNew implements Interceptor {
 
 	private static final Logger logger = LoggerFactory.getLogger(AuditLogInterceptorNew.class);
 	
+//	@Autowired
+//	private AuditEntitys auditEntitys;	
 	@Autowired
-	private AuditEntitys auditEntitys;	
+	private HashMap<String, Map<String, String>> auditEntitys;
 	
 	@Override
 	public Object intercept(Invocation invocation) throws Throwable {
@@ -31,7 +38,29 @@ public class AuditLogInterceptorNew implements Interceptor {
 		SqlCommandType commandType = mappedStatement.getSqlCommandType();
 		logger.info("AuditLogInterceptorNew:操作类型【{}】............",commandType);
 		
-		logger.info("审计列表为：【{}】", auditEntitys.getAuditEntitys());
+		Object obj = invocation.getArgs()[1];
+		
+		Class<? extends Object> clazz = obj.getClass();
+		String canonicalName = clazz.getCanonicalName();
+		
+		Method[] methods = clazz.getMethods();
+		for (Method method : methods) {
+			String methodName = method.getName();
+			if(methodName.startsWith("get") == false){
+				continue;
+			}
+			System.out.println(method.invoke(obj));
+		}
+		
+		Field[] fields = clazz.getFields();
+		
+		for (Field field : fields) {
+			String fieldTem = field.getName() + "=" + field.get(obj);
+			System.out.println(fieldTem);
+		}
+		
+		
+		logger.info("审计列表为：【{}】", auditEntitys);
 		
 		return invocation.proceed();
 	}
