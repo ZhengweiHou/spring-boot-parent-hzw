61cd739ac686c22a29985e7e29c2612733bece65
侯正伟 <houzw@allinfinance.com>
2018-08-29 18:01:33 +0800

    添加log4j2配置，对比测试

--- spring-boot-logback/src/main/java/com/hzw/learn/springboot/logback/RunStart.java
+++ spring-boot-logback/src/main/java/com/hzw/learn/springboot/logback/RunStart.java
@@ -4,11 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.hzw.learn.springboot.logback.runer1.LevelTest1;
-import com.hzw.learn.springboot.logback.runer2.LevelTest2;
-import com.hzw.learn.springboot.logback.runerother.ExceptionTest;
-import com.hzw.learn.springboot.logback.runerother.OutLogForHHH;
-import com.hzw.learn.springboot.logback.runerother.OutLogForWWW;
-import com.hzw.learn.springboot.logback.runerother.OutLogForZZZ;
 
 @Component
 public class RunStart implements CommandLineRunner{
@@ -16,11 +11,20 @@ public class RunStart implements CommandLineRunner{
 	@Override
 	public void run(String... arg0) throws Exception {
 		System.out.println("run start ...");
+//		System.err.println("13123123");
+//		try {
+//			ExceptionTest.createException();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		
 		new Thread(new LevelTest1()).start();
 //		new Thread(new LevelTest2()).start();
 //		new Thread(new ExceptionTest()).start();
-		new Thread(new OutLogForHHH()).start();
-		new Thread(new OutLogForZZZ()).start();
+//		new Thread(new OutLogForHHH()).start();
+//		new Thread(new OutLogForZZZ()).start();
 //		new Thread(new OutLogForWWW()).start();
 		
 	}
--- spring-boot-logback/src/main/java/com/hzw/learn/springboot/logback/runerother/ExceptionTest.java
+++ spring-boot-logback/src/main/java/com/hzw/learn/springboot/logback/runerother/ExceptionTest.java
@@ -14,11 +14,16 @@ public class ExceptionTest implements Runnable {
 		long sleep = 5*1000;
 		
 		while(true){
+			createException();
+			
 			try {
+				logger.info("创建错误。。。");
 				this.createException();
 			} catch (ArithmeticException e) {
+				logger.info("e.pringStack....");
 				e.printStackTrace(); // 此处日志不能输出到日志文件中
-				logger.error("ExceptionTest错误日志：",e); // 这样才能将异常输出到日志文件中
+//				logger.info("logger.error....");
+//				logger.error("ExceptionTest错误日志：",e); // 这样才能将异常输出到日志文件中
 			}
 			try {
 				Thread.sleep(sleep);
@@ -29,7 +34,7 @@ public class ExceptionTest implements Runnable {
 		
 	}
 	
-	private void createException() throws ArithmeticException{
+	public static void createException() throws ArithmeticException{
 		int a = 1/0;
 	}
 
