f815d9dfb683c044423d3f3e53b0393f8dde580e
侯正伟 <houzw@allinfinance.com>
2018-09-05 17:35:37 +0800

    mybatis

---
+++ spring-boot-mybatis/src/main/java/com/hzw/learn/springboot/mybatis/RunStart.java
@@ -0,0 +1,23 @@
+package com.hzw.learn.springboot.mybatis;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.hzw.learn.springboot.mybatis.dao.StudentDao;
+import com.hzw.learn.springboot.mybatis.model.Student;
+
+@Component
+public class RunStart implements CommandLineRunner{
+	@Autowired
+	private StudentDao studentDao;
+	
+	@Override
+	public void run(String... arg0) throws Exception {
+		System.out.println("run start ...");
+		studentDao.quetyStudentList(new Student());
+		
+	}
+
+}
