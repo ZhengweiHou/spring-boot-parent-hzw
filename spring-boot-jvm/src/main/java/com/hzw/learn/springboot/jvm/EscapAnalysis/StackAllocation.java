1408243e3ccb2d8718f2d0db0d8b1d755e105962
houzw <houzw@allinfinance.com>
2018-12-28 09:35:51 +0800

    123

---
+++ spring-boot-jvm/src/main/java/com/hzw/learn/springboot/jvm/EscapAnalysis/StackAllocation.java
@@ -0,0 +1,44 @@
+package com.hzw.learn.springboot.jvm.EscapAnalysis;
+
+/**
+ * 逃逸分析-栈分配
+ * @author houzw
+ */
+public class StackAllocation {
+	public static void main(String[] args) throws Exception {
+		int count = 2 * 100 * 100 * 100;
+		int sum = 0;
+		
+		for(int i=0 ; i < count ; i++) {
+			sum += test(i);
+			System.out.println(sum);
+		}
+		
+		Thread.sleep(50l);
+
+		System.in.read();
+	}
+	
+	public static int test(int age) {
+		/*
+		 User对象的作用域不会逃逸出test方法，当启用逃逸分析，标量替换优化会在栈上分配对象，而不会生成 User对象，减低GC的压力
+		 */
+		User usr = new User(age);
+		int age2 = usr.getAge();
+		return age2;
+	}
+}
+
+class User {
+	private int age;
+	
+	public User(int age) {
+		super();
+		this.age = age;
+	}
+
+	public int getAge() {
+		return age;
+	}
+	
+}
