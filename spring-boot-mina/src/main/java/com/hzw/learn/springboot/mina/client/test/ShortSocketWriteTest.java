743073f1c8d09fd3ddb311b59c6cde3abe943bf4
houzw <houzw@allinfinance.com>
2019-01-11 14:31:01 +0800

    短连接测试1

--- spring-boot-mina/src/main/java/com/hzw/learn/springboot/mina/client/test/ShortSocketWriteTest.java
+++ spring-boot-mina/src/main/java/com/hzw/learn/springboot/mina/client/test/ShortSocketWriteTest.java
@@ -2,6 +2,9 @@ package com.hzw.learn.springboot.mina.client.test;
 
 import java.util.Scanner;
 
+import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.transport.socket.nio.NioSocketConnector;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -11,33 +14,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.hzw.learn.springboot.mina.client.ClientApplication;
-import com.hzw.learn.springboot.mina.client.config.HzwSocketClient;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {ClientApplication.class})
-public class SocketWriteTest {
+public class ShortSocketWriteTest {
 	Logger log = LoggerFactory.getLogger("TEST");
 	
 	@Autowired
-	HzwSocketClient client;
+	NioSocketConnector socketConnector;
 	
 	@Test
-	public void writeTest() throws NumberFormatException, InterruptedException {
+	public void shortWriteTest() throws NumberFormatException, InterruptedException {
 		int n=0;
 
 		Scanner scan = new Scanner(System.in);
 		while(true) {
 			String str = scan.nextLine();
 //			int str = scan.nextInt();
-			System.out.println(">>长连接池测试>>>>>>>>>" + str);
+			System.out.println(">>短连接测试>>>>>>>>>" + str);
 			if("exit".equals(str)) {
 				break;
 			}
 			
-			new Thread(() -> {
-				String result = client.write("Hello Hzw, 这是长连接池客户端发送的消息，请求时间：" + System.currentTimeMillis() + "!");
-				log.info("服务端返回：{}", result);
-			}).start();
+			ConnectFuture connect = socketConnector.connect();
+			connect.awaitUninterruptibly();
+			IoSession session = connect.getSession();
+			session.write("Hello Hzw, 这是短连接客户端发送的消息，请求时间：" + System.currentTimeMillis() + "!");
+			session.getCloseFuture().awaitUninterruptibly();
+//			session.closeNow();
 		}
 
 //		Thread.sleep(new Long("300000"));
