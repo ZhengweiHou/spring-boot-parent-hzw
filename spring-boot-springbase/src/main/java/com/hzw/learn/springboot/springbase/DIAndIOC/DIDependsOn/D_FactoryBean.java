077ea31586fcd25167527ccbc5fd0fe7c698b67c
sirius <916108538@qq.com>
2022-03-11 17:07:34 +0800

    dependsOn test

--- spring-boot-springbase/src/main/java/com/hzw/learn/springboot/springbase/DIAndIOC/DIDependsOn/D_FactoryBean.java
+++ spring-boot-springbase/src/main/java/com/hzw/learn/springboot/springbase/DIAndIOC/DIDependsOn/D_FactoryBean.java
@@ -3,30 +3,31 @@ package com.hzw.learn.springboot.springbase.DIAndIOC.DIDependsOn;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.config.AbstractFactoryBean;
 
-public class D_FactoryBean extends AbstractFactoryBean<String>
-//        implements FactoryBean<String>
+public class D_FactoryBean implements FactoryBean<String>
+//        extends AbstractFactoryBean<String>
 {
     Object obj;
 
-//    @Override
-//    public String getObject() throws Exception {
-//        System.out.println(this.hashCode() + "=" + this.getClass().getSimpleName() + ": init !!");
-//        return "D_Bean:" + System.currentTimeMillis();
-//    }
+    @Override
+    public String getObject() throws Exception {
+        System.out.println(this.hashCode() + "=" + this.getClass().getSimpleName() + ": init !!");
+        return "D_Bean:" + System.currentTimeMillis();
+    }
 
     @Override
     public Class<?> getObjectType() {
         return FactoryBean.class;
     }
 
-    @Override
-    protected String createInstance() throws Exception {
-        System.out.println(this.hashCode() + "=" + this.getClass().getSimpleName() + ": init !!");
-        return "D_Bean:" + System.currentTimeMillis();
-    }
+//    @Override
+//    protected String createInstance() throws Exception {
+//        System.out.println(this.hashCode() + "=" + this.getClass().getSimpleName() + ": init !!");
+//        return "D_Bean:" + System.currentTimeMillis();
+//    }
 
     public void setObj(Object obj){
         this.obj = obj;
     }
+
 
 }
