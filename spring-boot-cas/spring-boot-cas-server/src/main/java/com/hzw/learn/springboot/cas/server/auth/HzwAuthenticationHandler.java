4551e17d1a647cffb20e2d1ebe4638def7c20df4
sirius <916108538@qq.com>
2022-04-02 11:56:15 +0800

    添加自定义的授权逻辑HzwAuthenticationHandler

---
+++ spring-boot-cas/spring-boot-cas-server/src/main/java/com/hzw/learn/springboot/cas/server/auth/HzwAuthenticationHandler.java
@@ -0,0 +1,50 @@
+package com.hzw.learn.springboot.cas.server.auth;
+
+import org.apereo.cas.authentication.AuthenticationHandlerExecutionResult;
+import org.apereo.cas.authentication.Credential;
+import org.apereo.cas.authentication.PreventedException;
+import org.apereo.cas.authentication.UsernamePasswordCredential;
+import org.apereo.cas.authentication.handler.support.AbstractPreAndPostProcessingAuthenticationHandler;
+import org.apereo.cas.authentication.principal.PrincipalFactory;
+import org.apereo.cas.services.ServicesManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.GeneralSecurityException;
+
+/**
+ * @ClassName HzwAuthenticationHandler
+ * @Description TODO
+ * @Author houzw
+ * @Date 2022/4/2
+ **/
+public class HzwAuthenticationHandler extends AbstractPreAndPostProcessingAuthenticationHandler {
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+
+    public HzwAuthenticationHandler(String name, ServicesManager servicesManager, PrincipalFactory principalFactory, Integer order) {
+        super(name, servicesManager, principalFactory, order);
+    }
+
+    protected AuthenticationHandlerExecutionResult doAuthentication(Credential credential) throws GeneralSecurityException, PreventedException {
+        log.info("==doAuthentication==>");
+        UsernamePasswordCredential userCredential = (UsernamePasswordCredential)credential;
+        return createHandlerResult(credential,principalFactory.createPrincipal(userCredential.getUsername()));
+    }
+
+    public boolean preAuthenticate(Credential credential) {
+        log.info("==preAuthenticate==>");
+        return true;
+    }
+
+    public AuthenticationHandlerExecutionResult postAuthenticate(Credential credential, AuthenticationHandlerExecutionResult result) {
+        log.info("==postAuthenticate==>");
+        return result;
+    }
+
+    public boolean supports(Credential credential) {
+        // 是否支持认证
+        log.info("==supports2==>");
+        return true;
+    }
+}
---
+++ spring-boot-cas/spring-boot-cas-server/src/main/java/com/hzw/learn/springboot/cas/server/config/HzwAuthenticationConfiguration.java
@@ -0,0 +1,42 @@
+package com.hzw.learn.springboot.cas.server.config;
+
+import com.hzw.learn.springboot.cas.server.auth.HzwAuthenticationHandler;
+import org.apereo.cas.authentication.AuthenticationEventExecutionPlan;
+import org.apereo.cas.authentication.AuthenticationEventExecutionPlanConfigurer;
+import org.apereo.cas.authentication.AuthenticationHandler;
+import org.apereo.cas.authentication.principal.DefaultPrincipalFactory;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.services.ServicesManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName HzwAuthenticationConfiguration
+ * @Description TODO
+ * @Author houzw
+ * @Date 2022/4/2
+ **/
+@Configuration
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class HzwAuthenticationConfiguration implements AuthenticationEventExecutionPlanConfigurer {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired ServicesManager servicesManager;
+
+    @Bean
+    public AuthenticationHandler hzwAUthenticationHandler(){
+        log.info("==new hzwAUthenticationHandler==>");
+        return new HzwAuthenticationHandler(HzwAuthenticationHandler.class.getName(),servicesManager,new DefaultPrincipalFactory(),1);
+    }
+
+    public void configureAuthenticationExecutionPlan(AuthenticationEventExecutionPlan plan) {
+        log.info("==register hzwAUthenticationHandler==>");
+        plan.registerAuthenticationHandler(hzwAUthenticationHandler());
+
+    }
+}
