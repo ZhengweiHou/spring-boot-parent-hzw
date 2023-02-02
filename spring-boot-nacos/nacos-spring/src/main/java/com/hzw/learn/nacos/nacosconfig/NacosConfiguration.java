4f7a5fa818770f72ba72b6f30dfd100e088b266f
sirius <916108538@qq.com>
2023-02-02 15:36:06 +0800

    nacos test

--- spring-boot-nacos/nacos-spring/src/main/java/com/hzw/learn/nacos/nacosconfig/NacosConfiguration.java
+++ spring-boot-nacos/nacos-spring/src/main/java/com/hzw/learn/nacos/nacosconfig/NacosConfiguration.java
@@ -8,10 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "127.0.0.1:8848",namespace = "public"))
-@EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "127.0.0.1:40905",namespace = "test"))
-//@EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "192.168.104.139:8848",namespace = "test"))
-//@EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "10.252.22.31:8848",namespace = "test"))
+// nacos地址可以通过-D方式指定，如：-Dnacos.server-addr=127.0.0.1:8148 -Dnacos.namespace=test
+@EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "127.0.0.1:8848",namespace = "test"))
 @NacosPropertySource(dataId = "test",groupId = "test",autoRefreshed = true)
 public class NacosConfiguration {
 //    @Bean
