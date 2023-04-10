9d4a531c055bfd799c95c831958906b2178392d2
sirius <916108538@qq.com>
2023-04-10 17:33:27 +0800

    BeanDefinitionRegistryPostProcessor test

--- spring-boot-springbase/src/main/java/com/hzw/learn/springboot/springbase/DIAndIOC/IocLifecycle/PostProcessor/HzwBeanDefinitionRegistryPostProcessor.java
+++ spring-boot-springbase/src/main/java/com/hzw/learn/springboot/springbase/DIAndIOC/IocLifecycle/PostProcessor/HzwBeanDefinitionRegistryPostProcessor.java
@@ -3,9 +3,12 @@ package com.hzw.learn.springboot.springbase.DIAndIOC.IocLifecycle.PostProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+
+import java.util.Arrays;
 
 public class HzwBeanDefinitionRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor {
 
@@ -14,6 +17,12 @@ public class HzwBeanDefinitionRegistryPostProcessor implements BeanDefinitionReg
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
         System.out.println("2=HzwBeanDefinitionRegistryPostProcessor.postProcessBeanDefinitionRegistry");
         log.debug("2=HzwBeanDefinitionRegistryPostProcessor.postProcessBeanDefinitionRegistry");
+        Arrays.stream(registry.getBeanDefinitionNames()).forEach(name ->{
+            BeanDefinition df = registry.getBeanDefinition(name);
+            if(df.getBeanClassName().equals("Hzw")){ // 移除满足条件的BeanDefinition
+                registry.removeBeanDefinition(name);
+            }
+        });
     }
 
     @Override
